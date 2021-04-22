cea69fe27e807ba9a150b6c370d4d36c588b9691
Oz N Tiram <oz.tiram@gmail.com>
2021-04-22 11:44:02 +0200

    Add long_description for the package
    Readable in pypi.
    
    Signed-off-by: Oz Tiram <oz.tiram@gmail.com>

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
+from os import path
+
 from setuptools import setup
+
+this_directory = path.abspath(path.dirname(__file__))
+
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 setup(
       name='make-help-helper',
-      version='0.1',
+      version='0.1.1',
       description="Help menus generator for Makefiles",
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       author='Oz N Tiram',
       author_email='oz.tiram@gmail.com',
       url='https://github.com/oz123/help-make-helper',
