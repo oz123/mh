e34471d3f96c8818318865b2cbc896d02a3865b2
Oz N Tiram <oz.tiram@gmail.com>
2021-07-05 10:57:42 +0200

    Put target names in the Queue
    Still not much to see though ...
    
    Signed-off-by: Oz Tiram <oz.tiram@gmail.com>

--- mh.c
+++ mh.c
@@ -3,6 +3,7 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <pcre2.h>
 #include "queue.h"
 
@@ -16,43 +17,24 @@ main(int argc, char *argv[])
 
     PCRE2_SPTR pattern = (PCRE2_SPTR)target_help;
     pcre2_code *re;
-    PCRE2_SPTR subject;
+
     int errornumber;
 
     PCRE2_SIZE erroroffset;
-    Queue* targets = NULL;
-    targets = queue_new();
+    PCRE2_SIZE *ovector;
+ 
+    Queue* targets = queue_new();
+    Queue* globals = queue_new();
 
     typedef struct target {
       char *name;
       char *help;
+      Queue *locals;
     } target_t;
     
-    target_t * a_target = NULL;
-    target_t * b_target = NULL;
-    target_t * c_target = NULL;
-    QueueValue item = NULL;
-    a_target = (target_t *) malloc(sizeof(target_t));
-    a_target->name = "foo";
-    a_target->help = "bar foo";
-    b_target = (target_t *) malloc(sizeof(target_t));
-    b_target->name = "foo 2";
-    b_target->help = "bar foo 2";
-    c_target = (target_t *) malloc(sizeof(target_t));
-    c_target->name = "foo 3";
-    c_target->help = "bar foo 3";
-    
-    queue_push_tail(targets, a_target);
-    queue_push_tail(targets, b_target);
-    queue_push_tail(targets, c_target);
-    
-    while (queue_is_empty(targets) == 0) {
-	target_t *i = queue_pop_head(targets); 
-	printf("target %s, help %s\n", i->name, i->help);
-    }
     re = pcre2_compile(
-		    pattern,
-		    PCRE2_ZERO_TERMINATED, /* indicates pattern is zero-terminated */
+                    pattern,
+                    PCRE2_ZERO_TERMINATED, /* indicates pattern is zero-terminated */
                     0,                     /* default options */
                     &errornumber,          /* for error number */
                     &erroroffset,          /* for error offset */
@@ -86,18 +68,35 @@ main(int argc, char *argv[])
              case PCRE2_ERROR_NOMATCH: printf("No match\n"); break;
              /* Handle other special cases if you like */
              default: printf("Matching error %d\n", rc); break;
+             }
        }
 
-       pcre2_match_data_free(match_data);   /* Release memory used for the match */
-       pcre2_code_free(re);                 /* data and the compiled pattern. */
+       printf("rc %d\n", rc);
+       ovector = pcre2_get_ovector_pointer(match_data);
+       printf("\nMatch succeeded at offset %d\n", (int)ovector[0]);
+       const char *match;
+       int rc2 = 0;
+       PCRE2_SPTR substring_start = 0;
+       size_t substring_length = 0;
+       target_t *a_target = (target_t *) malloc(sizeof(target_t));
+       for (int i = 0; i < rc; i++) {
+           substring_start = line + ovector[2*i];
+           substring_length = ovector[2*i+1] - ovector[2*i];
+           a_target->name = malloc(substring_length + 1);
+           strncpy(a_target->name, substring_start, substring_length);
+           a_target->name[substring_length] = '\0';
+           queue_push_tail(targets, a_target);
        }
-
     }
 
+    while (queue_is_empty(targets) == 0) {
+	target_t *i = queue_pop_head(targets);
+        printf("target %s, help %s\n", i->name, i->help);
+    }
 
-    free(a_target);
-    free(b_target);
-    free(c_target);
+    pcre2_match_data_free(match_data);   /* Release memory used for the match */
+    pcre2_code_free(re);                 /* data and the compiled pattern. */
+
     queue_free(targets);
     free(line);
     exit(EXIT_SUCCESS);
