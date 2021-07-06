dac012c9b04006887aa7573ed8d7d5998df4b2b3
Oz N Tiram <oz.tiram@gmail.com>
2021-07-06 19:45:00 +0200

    Pseudo code to complete the task
    Still need to implement this ...
    
    Signed-off-by: Oz Tiram <oz.tiram@gmail.com>

--- mc.h
+++ mc.h
@@ -9,9 +9,29 @@ typedef struct target {
   Queue *locals;
 } target_t;
 
+/**
+ * variable global or local
+ */
+typedef struct variable {
+  char *name;
+  char *default_value;
+  char *help;
+} variable_t;
 
+/**
+ * Check that a line in file is in the form of <traget-name: ## help for the target>
+ * <target-name>: <var-name> ?= <default value> #? <help string>
+ * <var-name> ?= <default value>#? <help string>
+ */
+int check_line_for_global_var(char *line, variable_t *global);
+
+/**
+ * Check that a line in file is in the form of <traget-name: ## help for the target>
+ * <target-name>: <var-name> ?= <default value> #? <help string>
+ */
+int check_line_for_local_var(char *line, variable_t *local);
 
 /**
  * Check that a line in file is in the form of <traget-name: ## help for the target>
  */
-target_t* check_line_for_target(char *line);
+int check_line_for_target(char *line, target_t *target);
